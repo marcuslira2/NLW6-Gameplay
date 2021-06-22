e8484f8769707d0374a4109b3bc0c3d2b0beb3e5
Marcus Lira <marcuslira57@gmail.com>
2021-06-22 03:50:37 -0300

    Signin Screen

---
+++ src/screens/signin/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+import { theme} from "../../global/styles/theme";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.color.background,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    image: {
+        width: '100%',
+        height: 360,
+    },
+    content:{
+        marginTop:-40,
+        paddingHorizontal: 50,
+    },
+    title:{
+        color: theme.color.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+
+    },
+    subtittle:{
+        color: theme.color.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
+    }
+});
